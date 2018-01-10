3db02f1abc78c5308fa5f59b89e744cd7c27afcb
prototypeheroku <34549250+prototypeheroku@users.noreply.github.com>
2018-01-10 17:30:57 +0900

    Update index.blade.php

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -13,25 +13,7 @@
     <!-- table -->
     <table class="table table-striped">
 
-    <!-- loop -->
-    @foreach($users as $user)
-        <tr>
-            <td>{{$user->id}}</td>
-            <td>{{$user->name}}</td>
-            <td>{{$user->email}}</td>
-            <td><a href="/users/show/{{$user->id}}" class="btn btn-primary btn-sm">詳細</a></td>
-            <td><a href="/users/edit/{{$user->id}}" class="btn btn-primary btn-sm">編集</a></td>
-            <td>
-                <form method="post" action="/users/destroy/{{$user->id}}">
-                    <input type="hidden" name="_token" value="{{csrf_token()}}">
-                    <input type="submit" value="削除" class="btn btn-danger btn-sm btn-destroy">
-                </form>
-            </td>
-        </tr>
-    @endforeach
     </table>
 
-    <!-- page control -->
-    {!! $users->render() !!}
 
 @stop

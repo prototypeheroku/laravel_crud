ef78adf80ce5a6d5f1f4f5f224c5d56623a757b5
prototypeheroku <34549250+prototypeheroku@users.noreply.github.com>
2018-01-10 17:17:39 +0900

    Create index.blade.php

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,20 @@
+@extends('layout')
+
+@section('content')
+
+    <h1>一覧表示</h1>
+
+    <table class="table table-striped">
+    @foreach($users as $user)
+        <tr>
+            <td>{{$user->id}}</td>
+            <td>{{$user->name}}</td>
+            <td>{{$user->email}}</td>
+        </tr>
+    @endforeach
+    </table>
+
+    <!-- page control -->
+    {!! $users->render() !!}
+
+@stop

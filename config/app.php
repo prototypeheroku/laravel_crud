61e7c3aaab03d45d7840c60c95a9e1ed489f9fb1
prototypeheroku <34549250+prototypeheroku@users.noreply.github.com>
2018-01-10 16:56:22 +0900

    Update app.php

--- config/app.php
+++ config/app.php
@@ -152,6 +152,7 @@ return [
         Illuminate\Filesystem\FilesystemServiceProvider::class,
         Illuminate\Foundation\Providers\FoundationServiceProvider::class,
         Illuminate\Hashing\HashServiceProvider::class,
+        'Collective\Html\HtmlServiceProvider',
         Illuminate\Mail\MailServiceProvider::class,
         Illuminate\Notifications\NotificationServiceProvider::class,
         Illuminate\Pagination\PaginationServiceProvider::class,

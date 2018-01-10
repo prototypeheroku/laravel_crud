c6b032d7adc43f8d25954351c44d12c3629fd39a
prototypeheroku <34549250+prototypeheroku@users.noreply.github.com>
2018-01-10 14:13:43 +0900

    Update Article.php

--- app/Article.php
+++ app/Article.php
@@ -5,6 +5,18 @@
   class Article extends Model
   {
   
-      //
+      /**
+       * The table associated with the model.
+       *
+       * @var string
+       */
+      protected $table = 'articles';
+  
+      /**
+       * The attributes that are mass assignable.
+       *
+       * @var array
+       */
+      protected $fillable = ['title', 'body'];
   
   }
